5f9c8ef11a78d17bd59378bc123e342d478d6623
kipkirui63 <kiprutonyachire@gmail.com>
2023-07-07 18:35:23 +0300

    added readme files

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # farmSolution
+# Farm Data Analysis
+
+## Description
+
+Farm Data Analysis is a website that serves as a platform for storing farmers' data and analyzing farm inputs. It provides recommendations on how farmers can improve their farming practices and optimize nutrient levels for better crop or livestock production. The website offers features such as data storage, analysis tools, and nutrient recommendations to help farmers make informed decisions about their farming operations.
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Technology Used](#technology-used)
+- [References](#references)
+- [License](#license)
+- [Author's Info](#authors-info)
+
+## Installation
+
+### Requirements
+
+- Computer, phone, tablet, or iPad
+- Internet access
+
+### Installation Process
+
+To access the Farm Data Analysis website, follow these steps:
+
+1. Visit the following URL: [Farm Data Analysis Website](https://feedsandnutrients.onrender.com)
+2. You can also find the project repository on GitHub: [Farm Data Analysis Repository](https://github.com/kipkirui63/farmSolution)
+
+## Technology Used
+
+The Farm Data Analysis website was built using the following technologies:
+
+- HTML: Used to build the structure of the pages.
+- CSS: Used to style the pages and create an intuitive user interface.
+- JavaScript: Used for interactivity and implementing data analysis functionalities.
+- JSON: Used for data storage and retrieval.
+
+## References
+
+The Farm Data Analysis website may reference external libraries or frameworks for specific functionalities.
+
+## License
+
+This project is licensed under the MIT License.
+
+## Author's Info
+
+Enock Sang
+Email: enocksang8356@gmail.com
+
--- script.js
+++ script.js
@@ -1,57 +1,155 @@
+// Fetch the feeds data from the API
+fetch('https://feedsandnutrients.onrender.com/feeds')
+  .then(response => response.json())
+  .then(data => {
+    const calculateNutrients = () => {
+      // Get the feed inputs
+      const feedInputs = [
+        document.getElementById('feedInput1').value,
+        document.getElementById('feedInput2').value,
+        document.getElementById('feedInput3').value,
+        document.getElementById('feedInput4').value,
+        document.getElementById('feedInput5').value,
+        document.getElementById('feedInput6').value
+      ];
+
+      // Calculate feed nutrients
+      const feedNutrients = calculateFeedNutrients(feedInputs, data.feeds);
+
+      // Display feed nutrients in the list
+      displayFeedNutrients(feedNutrients);
+
+      // Calculate nutrient recommendations
+      const nutrientRecommendations = calculateNutrientRecommendations(feedNutrients, data.feeds);
+
+      // Display nutrient recommendations in the list
+      displayNutrientRecommendations(nutrientRecommendations);
+    };
+
+    // Add event listener to the "Calculate" button
+    document.getElementById('calculateButton').addEventListener('click', calculateNutrients);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+
+// Function to calculate feed nutrients and nutrient recommendations
 function calculateNutrients() {
-    // Get the feed input value
-    const feedInput = document.getElementById('feedInput').value;
-  
-    // Perform nutrient calculations (simplified example)
-    const nutrientComposition = calculateNutrientComposition(feedInput);
-  
-    // Display feed nutrients
-    const feedNutrientsList = document.getElementById('feedNutrientsList');
-    feedNutrientsList.innerHTML = '';
-    for (const nutrient in nutrientComposition) {
-      const listItem = document.createElement('li');
-      listItem.textContent = `${nutrient}: ${nutrientComposition[nutrient]}`;
-      feedNutrientsList.appendChild(listItem);
-    }
-  
-    // Get soil nutrient recommendations based on feed composition (simplified example)
-    const soilRecommendations = getSoilRecommendations(nutrientComposition);
-  
-    // Display soil nutrient recommendations
-    const soilRecommendationsList = document.getElementById('soilRecommendationsList');
-    soilRecommendationsList.innerHTML = '';
-    for (const nutrient in soilRecommendations) {
-      const listItem = document.createElement('li');
-      listItem.textContent = `${nutrient}: ${soilRecommendations[nutrient]}`;
-      soilRecommendationsList.appendChild(listItem);
-    }
-  }
-  
-  // Example nutrient composition calculation function (replace with your actual logic)
-  function calculateNutrientComposition(feedInput) {
-    // Perform nutrient calculations based on the feed input
-    // Replace this with your own implementation
-    const nutrientComposition = {
-      nitrogen: 10,
-      phosphorus: 5,
-      potassium: 8,
-      calcium: 3
-    };
-  
-    return nutrientComposition;
-  }
-  
-  // Example soil nutrient recommendations function (replace with your actual logic)
-  function getSoilRecommendations(nutrientComposition) {
-    // Get soil nutrient recommendations based on the feed nutrient composition
-    // Replace this with your own implementation
-    const soilRecommendations = {
-      nitrogen: nutrientComposition.nitrogen * 1.5,
-      phosphorus: nutrientComposition.phosphorus * 0.8,
-      potassium: nutrientComposition.potassium * 1.2,
-      calcium: nutrientComposition.calcium * 0.9
-    };
-  
-    return soilRecommendations;
-  }
-  
+  // Get the user input values
+  const feedInput1 = document.getElementById("feedInput1").value;
+  const feedInput2 = document.getElementById("feedInput2").value;
+  const feedInput3 = document.getElementById("feedInput3").value;
+  const feedInput4 = document.getElementById("feedInput4").value;
+  const feedInput5 = document.getElementById("feedInput5").value;
+  const feedInput6 = document.getElementById("feedInput6").value;
+
+  // Perform the calculations
+  const feedNutrients = {
+    carbohydrates: (feedInput1 / 100) * 100,
+    protein: (feedInput2 / 100) * 100,
+    fiber: (feedInput3 / 100) * 100,
+    vitamins: (feedInput4 / 100) * 100,
+    minerals: (feedInput5 / 100) * 100,
+    fats: (feedInput6 / 100) * 100,
+  };
+
+  const nutrientRecommendations = {
+    // Define the nutrient recommendations based on the balanced diet
+    // Replace with your actual nutrient recommendations
+
+    carbohydrates: "Carbohydrates Recommendation",
+    protein: "Protein Recommendation",
+    fiber: "Fiber Recommendation",
+    vitamins: "Vitamins Recommendation",
+    minerals: "Minerals Recommendation",
+    fats: "Fats Recommendation",
+  };
+
+  // Display the feed nutrients in the output textarea
+  document.getElementById("feedOutputDisplay").value = JSON.stringify(feedNutrients);
+
+  // Display the nutrient recommendations in the output textarea
+  document.getElementById("nutrientsOutputDisplay").value = JSON.stringify(nutrientRecommendations);
+}
+// Function to calculate feed nutrients and nutrient recommendations
+function calculateNutrients() {
+  // Get the user input values
+  const feedInput1 = document.getElementById("feedInput1").name;
+  const feedInput2 = document.getElementById("feedInput2").value;
+  const feedInput3 = document.getElementById("feedInput3").value;
+  const feedInput4 = document.getElementById("feedInput4").value;
+  const feedInput5 = document.getElementById("feedInput5").value;
+  const feedInput6 = document.getElementById("feedInput6").value;
+
+  // Perform the calculations
+  const feedNutrients = {
+    carbohydrates: (feedInput1 / 100) * 100,
+    protein: (feedInput2 / 100) * 100,
+    fiber: (feedInput3 / 100) * 100,
+    vitamins: (feedInput4 / 100) * 100,
+    minerals: (feedInput5 / 100) * 100,
+    fats: (feedInput6 / 100) * 100,
+  };
+
+  const nutrientRecommendations = {
+    // Define the nutrient recommendations based on the balanced diet
+    // Replace with your actual nutrient recommendations
+
+    carbohydrates: "Carbohydrates Recommendation",
+    protein: "Protein Recommendation",
+    fiber: "Fiber Recommendation",
+    vitamins: "Vitamins Recommendation",
+    minerals: "Minerals Recommendation",
+    fats: "Fats Recommendation",
+  };
+
+  // Display the feed nutrients in the output textarea
+  document.getElementById("feedOutputDisplay").value = JSON.stringify(feedNutrients);
+
+  // Display the nutrient recommendations in the output textarea
+  document.getElementById("nutrientsOutputDisplay").value = JSON.stringify(nutrientRecommendations);
+}
+
+
+
+
+
+
+// Function to add a new animal row
+function addAnimalRow() {
+  const animalsContainer = document.getElementById('animalsContainer');
+
+  // Create a new row div
+  const row = document.createElement('div');
+  row.classList.add('animal-row');
+
+  // Create input fields for animal type and count
+  const animalTypeInput = document.createElement('input');
+  animalTypeInput.type = 'text';
+  animalTypeInput.placeholder = 'Animal Type';
+  animalTypeInput.classList.add('animal-type-input');
+
+  const animalCountInput = document.createElement('input');
+  animalCountInput.type = 'text';
+  animalCountInput.placeholder = 'conditions summary';
+  animalCountInput.classList.add('animal-count-input');
+
+  // Create a delete button
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.classList.add('delete-button');
+  deleteButton.addEventListener('click', () => {
+    animalsContainer.removeChild(row);
+  });
+
+  // Append the input fields and delete button to the row
+  row.appendChild(animalTypeInput);
+  row.appendChild(animalCountInput);
+  row.appendChild(deleteButton);
+
+  // Append the row to the animals container
+  animalsContainer.appendChild(row);
+}
+
+// Add event listener to the "Add Animal" button
+document.getElementById('addAnimalButton').addEventListener('click', addAnimalRow);
